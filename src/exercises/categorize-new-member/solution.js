627c174c00d96fd01f6a25224e83c01a83adf6a5
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-26 11:03:17 -0300

    feat(exercises): categorize new member
    INstead of declaring new constants, I just destructure the current value

--- src/exercises/categorize-new-member/solution.js
+++ src/exercises/categorize-new-member/solution.js
@@ -6,4 +6,10 @@ function openOrSenior(data) {
   });
 }
 
+function openOrSenior(data) {
+  return data.map(([age, handicap]) => {
+    return (age > 54 && handicap > 7) ? 'Senior' : 'Open';
+  });
+}
+
 console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]));
