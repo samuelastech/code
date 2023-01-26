a9bc8aabd623de5fa820a9653ec998df058a0e7c
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-26 10:24:44 -0300

    feat(exercises): even or odd

---
+++ src/exercises/even-or-odd/solutions.js
@@ -0,0 +1,8 @@
+function evenOrOdd(number) {
+  return +number % 2 === 0 ? 'Even' : 'Odd';
+}
+
+console.log(evenOrOdd(1));
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(3));
+console.log(evenOrOdd(4));
