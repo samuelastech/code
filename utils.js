e1e0b5427771e6cb179e94382e96369256aee0ab
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-06-17 14:50:47 -0300

    feat: generate random matrix

--- utils.js
+++ utils.js
@@ -1,8 +1,19 @@
-function getRandomInt(max) {
+/**
+ * Generates a random int in between the max param
+ * @param {number} max size of the integer
+ * @returns {number}
+ */
+function getRandomInt(max = 100) {
   return Math.floor(Math.random() * max);
 }
 
-function generateRandomNumbers(max, size) {
+/**
+ * Generates an array with random int
+ * @param {number} max size of each integer
+ * @param {number} size is the length of the array
+ * @returns {Array<number>}
+ */
+function generateRandomNumbers(max = 100, size = 10) {
   const nums = [];
   
   for (let i = 0; i < size; i++) {
@@ -12,5 +23,29 @@ function generateRandomNumbers(max, size) {
   return nums;
 }
 
+/**
+ * Generates a random matrix
+ * @param {number} m rows
+ * @param {number} n columns
+ * @param {number} max value of each integer
+ */
+function generateMatrix(m = 10, n = 10, max = 100) {
+  const matrix = [];
+
+  for (let i = 0; i < m; i++) {
+    const row = [];
+
+    for (let j = 0; j < n; j++) {
+      row.push(getRandomInt(max));
+    }
+    
+    matrix.push(row);
+  }
+
+  return matrix;
+}
+
+exports.getRandomInt = getRandomInt;
 exports.generateRandomNumbers = generateRandomNumbers;
+exports.generateMatrix = generateMatrix;
 exports.myNums = [13,11,9,1,3,2508,456,7,8,21];
