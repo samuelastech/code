c064988a7569c42a2f0fd7968cc8b0d6fbbe314c
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-06-20 11:38:04 -0300

    feat(math): perimeter and area

---
+++ math/Math.js
@@ -0,0 +1,23 @@
+class Math {
+  /**
+   * Returns the area of a 4-sided shape
+   * @param {number} length 
+   * @param {number} width
+   * @returns {number} area
+   */
+  static getArea = (length, width) => {
+    return length * width;
+  }
+
+  /**
+   * Returns the perimeter of a 4-sided shape
+   * @param {number} length 
+   * @param {number} width 
+   * @returns {number} perimeter
+   */
+  static getPerimeter = (length, width) => {
+    return (2 * length) + (2 * width);
+  }
+}
+
+exports.Math = Math;
