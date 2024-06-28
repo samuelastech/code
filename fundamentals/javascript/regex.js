628b9d410d24b32820d193e90aeb2b9988387d75
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-06-28 13:47:48 -0300

    feat(regex): quantifier

--- fundamentals/javascript/regex.js
+++ fundamentals/javascript/regex.js
@@ -17,11 +17,16 @@ console.log("'Militar' ends with a vowel?", regex.test("Militar"));
 console.log("\n");
 
 /**
- * Referencing a group
+ * Referencing a group \1 \2 \n
  */
 regex = /^(a).*\1$/i;
 console.log("'Ana' starts and ends with 'A'?", regex.test("Ana"));
 
 regex = /^(a)(b).*\2$/i;
 console.log("'Abnab' ends with 'B'?", regex.test("Abnab"));
+console.log("\n");
+
+/** Checking a number of characters \d{2} a{3} sa{n} */
+regex = /a{4}/i;
+console.log("'AAAA' repeats four times?", regex.test("AAAA"));
 console.log("\n");
