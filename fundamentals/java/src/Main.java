39d37deba0ca4a27a3f0bc748db220a7756f1ab9
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-08-10 19:45:31 -0300

    feat(java): data types

---
+++ fundamentals/java/src/Main.java
@@ -0,0 +1,7 @@
+import variables.Variables;
+
+public class Main {
+    public static void main(String[] args) {
+        Variables.run();
+    }
+}
---
+++ fundamentals/java/src/variables/Variables.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class Variables {
+    static public void run() {
+        byte byteIntNumber = 127;
+        short shortIntNumber = 32_767;
+        int intNumber = 2_147_483_647;
+        long longIntNumber = 10; // 9.223.372.036.854.775.807
+
+        float shorterDecimalNumber = 8.4F;
+        double biggerDecimalNumber = 8.4;
+
+        boolean isValid = false;
+
+        char letter = 'a';
+        String sentence = "merry song";
+        System.out.println("All variables are declared");
+    }
+}
