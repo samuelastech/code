e1a491200fecbec878db0c981e8ccbaaf5dd099e
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-08-25 16:25:37 -0300

    fix(data-structure): circular queue logic

--- data-structure/circular-queue/src/CircularQueue.java
+++ data-structure/circular-queue/src/CircularQueue.java
@@ -1,29 +1,34 @@
 public class CircularQueue<T> {
     private T[] elements;
-    private int start, end, size;
+    private int start, end, size, count;
 
     public CircularQueue(int size) {
         this.elements = (T[]) new Object[size];
         this.start = 0;
-        this.end = -1;
+        this.end = 0;
+        this.count = 0;
         this.size = size;
     }
 
-    public void add(T value) {
-        if (!isFull()) {
-            end = (end + 1) % size;
-            elements[end] = value;
+    public void add(T value) throws Exception {
+        if (isFull()) {
+            throw new Exception("The queue is full");
         }
+
+        elements[end] = value;
+        end = (end + 1) % size;
+        count++;
     }
 
     public T remove() throws Exception {
-        if (!isEmpty()) {
-            T value = elements[start];
-            start = (start + 1) % size;
-            return value;
+        if (isEmpty()) {
+            throw new Exception("The queue is empty");
         }
 
-        throw new Exception("The queue is empty");
+        T value = elements[start];
+        start = (start + 1) % size;
+        count--;
+        return value;
     }
 
     public boolean isEmpty() {
@@ -31,6 +36,6 @@ public class CircularQueue<T> {
     }
 
     public boolean isFull() {
-        return end == (start - 1) % size;
+        return start == (end + 1) % size;
     }
 }
--- data-structure/circular-queue/src/Main.java
+++ data-structure/circular-queue/src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        CircularQueue<Integer> q = new CircularQueue<Integer>(10);
-        q.add(10);
-        q.add(21);
-        System.out.println(q.isEmpty());
-
+        CircularQueue<Integer> q = new CircularQueue<Integer>(5);
         try {
-            q.remove();
+            q.add(10);
+            q.add(21);
+            q.add(21);
+            q.add(21);
         } catch (Exception e) {
             System.out.println(e);
         }
