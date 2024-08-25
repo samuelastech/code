3e9d1f105f45a2bfa308c53d7952646e61e47ef3
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-08-25 14:23:30 -0300

    feat(java): deadlock example

---
+++ fundamentals/java/src/deadlock/Deadlock.java
@@ -0,0 +1,43 @@
+package deadlock;
+
+public class Deadlock {
+    public static void main(String[] args) {
+        final Object resource1 = "Resource 1";
+        final Object resource2 = "Resource 2";
+
+        Thread t1 = new Thread(() -> {
+            synchronized (resource1) {
+                System.out.println("Thread 1: locked resource 1");
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (resource2) {
+                    System.out.println("Thread 1: locked resource 2");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            synchronized (resource2) {
+                System.out.println("Thread 2: locked resource 2");
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (resource1) {
+                    System.out.println("Thread 2: locked resource 1");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
