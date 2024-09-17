9ddf88fed41390c9dd074f38465c973f02707006
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-09-17 20:54:33 -0300

    feat(go): concatenation

---
+++ fundamentals/go/concatenation.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Samuel"
+	var age int = 99
+	var weight float32 = 67.5
+
+	fmt.Println("Hello", name, "I know your age is", age, "and your weight is", weight)
+}
